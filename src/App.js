ef37d2e56c0693783d3ac6660285805ce103fc97
kiran <krmate634@gmail.com>
2021-09-28 23:19:47 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "./styles/output.css";
+import { useEffect, useState } from "react";
+import { Footer } from "./components";
+function App() {
+  const [theme, setTheme] = useState(true);
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    if (theme) {
+      root.classList.add("dark");
+    } else {
+      root.classList.remove("dark");
+      root.classList.add("light");
+    }
+  }, [theme]);
+  return (
+    <div className="App">
+      <button
+        id="switchTheme"
+        onClick={() => setTheme(!theme)}
+        class="h-10 w-10 flex justify-center items-center focus:outline-none text-yellow-500"
+      >
+        <svg
+          class="w-6 h-6"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+      </button>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
